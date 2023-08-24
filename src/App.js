e1e2f39f11ee45a9279cb7c73a549f9723dae756
NingChu1998 <68261733+NingChu1998@users.noreply.github.com>
2023-08-23 23:02:56 -0400

    Added Semantic HTML, logo,nav elements and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-
 import './App.css';
+import React from 'react';
+import Nav from  "./components/Nav"
+import Header from  "./components/Header"
+import Main from  "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Second Exercise
-        </p>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Footer = () => {
+    return(
+        <footer>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Bookings</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
