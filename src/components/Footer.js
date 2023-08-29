35c17feaa1e29177dc60e0fbb18c18057b742e7f
NingChu1998 <68261733+NingChu1998@users.noreply.github.com>
2023-08-28 22:45:11 -0400

    Set up CSS Layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from "react"
-
+import small_logo from "../img/small_logo.png"
 const Footer = () => {
     return(
         <footer>
+            <section>
+            <div>
+                <img src={small_logo} />
+            </div>
+            <div>
+                <h3>Document Navigation</h3>
             <ul>
                 <li>Home</li>
                 <li>About</li>
@@ -11,6 +17,24 @@ const Footer = () => {
                 <li>Order Online</li>
                 <li>Login</li>
             </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+            <ul>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            </div>
+            <div>
+                <h3>Social Media Links</h3>
+            <ul>
+                <li>Address</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+            </ul>
+            </div>
+            </section>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
 import React from "react";
+import food1 from "../img/thai.webp"
 
 const Header = () => {
     return(
-        <header></header>
+        <header>
+            <section>
+            <div>
+            <h2>Little Lemon</h2>
+            <h3>Som Tum (Green Papaya Salad)</h3>
+            <p>WWe are a family-owned restaurant specializing in authentic Thai cuisine, where we blend time-honored recipes with a contemporary flair.</p>
+            <button>Reserve a Table</button>
+            </div>
+            <div>
+                <img src={food1} />
+            </div>
+            </section>
+        </header>
     )
 }
 
