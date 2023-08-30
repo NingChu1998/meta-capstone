ab2303a9e9bec8abc7cd42b4250cd3ffb88361cb
NingChu1998 <68261733+NingChu1998@users.noreply.github.com>
2023-08-29 23:46:12 -0400

    Added API & Modify Form

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const Booking = (props) => {
     return (
-        <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch}/>
+        <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} submitForm={props.submitForm}/>
     );
 }
 
